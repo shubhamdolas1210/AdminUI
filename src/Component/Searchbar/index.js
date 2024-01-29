135d647a6ad67dbfdea6330f042715de045538e5
shubham dolas <shubhamdolas1210@gmail.com>
2024-01-29 21:14:08 +0530

    Searchbar Updated

---
+++ src/Component/Searchbar/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Searchbar = () => {
+  return (
+    <div className="Searchbar">
+      <input type="text" placeholder="Search by name, email or role" />
+    </div>
+  );
+};
+
+export default Searchbar;
